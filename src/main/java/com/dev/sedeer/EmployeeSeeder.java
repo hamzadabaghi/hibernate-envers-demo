9711e9d8dfbac4f8656ec7db116b7168f19250e4
Hamza DABAGHI <hdabaghi@leyton.com>
2022-08-08 10:52:46 +0100

    Insert operation - REVTYPE : 0.

--- src/main/java/com/dev/sedeer/EmployeeSeeder.java
+++ src/main/java/com/dev/sedeer/EmployeeSeeder.java
@@ -1,23 +1,46 @@
 package com.dev.sedeer;
 
+import com.dev.entity.Company;
 import com.dev.entity.Employee;
-import com.dev.repo.EmployeeRepo;
+import com.dev.repo.CompanyRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Component
 public class EmployeeSeeder implements CommandLineRunner {
 
-    private final EmployeeRepo employeeRepo;
+    private final CompanyRepo companyRepo;
 
     @Override
     public void run(String... args) {
 
-        employeeRepo.saveAll(List.of(new Employee(null,"Hamza","Soukaina","Fez",null),
-                new Employee(null,"Omar","Casa","Casa",null) ));
+        Company company = new Company();
+        company.setName("E Corp");
+        company.setCity("New York City");
+        company.setStreet(null);
+
+        Set<Employee> employees = new HashSet<>();
+
+        Employee employee = new Employee();
+        employee.setCompany(company);
+        employee.setLastName("Spencer");
+        employee.setStreet("High Street 123");
+        employee.setCity("Newark");
+        employees.add(employee);
+
+        employee = new Employee();
+        employee.setCompany(company);
+        employee.setLastName("Ralbern");
+        employee.setStreet("57th Street");
+        employee.setCity("New York City");
+        employees.add(employee);
+
+        company.setEmployees(employees);
+
+        companyRepo.save(company);
     }
 }
